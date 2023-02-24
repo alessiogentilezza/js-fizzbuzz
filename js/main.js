367082e4b34af89621dee1bf6cddd10d30680989
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-02-24 15:52:23 +0100

    aggiunto un po di color

--- js/main.js
+++ js/main.js
@@ -2,19 +2,45 @@ const containerDom = document.querySelector('.container');
 
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz');
+        console.log('FizzBuzz');
+        const squareDom = document.createElement('div');
+        squareDom.classList.add('square');
+        squareDom.classList.add('red');
+        squareDom.append('FizzBuzz');
+        containerDom.append(squareDom);
+    
+
     } else if (i % 3 === 0) {
-      console.log('Buzz');
+        console.log('Fizz');
+        const squareDom = document.createElement('div');
+        squareDom.classList.add('square');
+        squareDom.classList.add('green');
+        squareDom.append('Fizz');
+        containerDom.append(squareDom);
+
+
     } else if (i % 5 === 0) {
-      console.log('Fizz');
+        console.log('Buzz');
+        const squareDom = document.createElement('div');
+        squareDom.classList.add('square');
+        squareDom.classList.add('orange');
+        squareDom.append('Buzz');
+        containerDom.append(squareDom);
+
     } else {
-      console.log(i);
+        console.log(i);
+        const squareDom = document.createElement('div');
+        squareDom.classList.add('square');
+        squareDom.append(i);
+        containerDom.append(squareDom);
+
+
     }
 
-    const squareDom = document.createElement('div');
-    squareDom.classList.add('square');
-    squareDom.append(i);
-    containerDom.append(squareDom);
+    // const squareDom = document.createElement('div');
+    // squareDom.classList.add('square');
+    // squareDom.append(i);
+    // containerDom.append(squareDom);
 
-  }
-  
+}
+
