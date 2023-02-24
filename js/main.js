bbfaa47ff0b788df474db2980718f13649d07f16
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-02-24 16:11:10 +0100

    esercizio completato e codice js pulito BONUS compreso

--- js/main.js
+++ js/main.js
@@ -1,46 +1,30 @@
 const containerDom = document.querySelector('.container');
 
 for (let i = 1; i <= 100; i++) {
+    const squareDom = document.createElement('div');
+    squareDom.classList.add('square');
+    containerDom.append(squareDom);
+
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuzz');
-        const squareDom = document.createElement('div');
-        squareDom.classList.add('square');
         squareDom.classList.add('red');
         squareDom.append('FizzBuzz');
-        containerDom.append(squareDom);
-    
+
 
     } else if (i % 3 === 0) {
         console.log('Fizz');
-        const squareDom = document.createElement('div');
-        squareDom.classList.add('square');
         squareDom.classList.add('green');
         squareDom.append('Fizz');
-        containerDom.append(squareDom);
 
 
     } else if (i % 5 === 0) {
         console.log('Buzz');
-        const squareDom = document.createElement('div');
-        squareDom.classList.add('square');
         squareDom.classList.add('orange');
         squareDom.append('Buzz');
-        containerDom.append(squareDom);
 
     } else {
         console.log(i);
-        const squareDom = document.createElement('div');
-        squareDom.classList.add('square');
         squareDom.append(i);
-        containerDom.append(squareDom);
-
-
     }
 
-    // const squareDom = document.createElement('div');
-    // squareDom.classList.add('square');
-    // squareDom.append(i);
-    // containerDom.append(squareDom);
-
 }
-
