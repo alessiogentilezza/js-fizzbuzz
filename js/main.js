d094ec66d038359d6115017044579fd2e5c1a8ae
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-02-24 15:38:16 +0100

    corretto codice js e inizio style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
+const containerDom = document.querySelector('.container');
+
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-      console.log('Fizz');
-    } else if (i % 5 === 0) {
-      console.log('Buzz');
-    } else if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Buzz');
+    } else if (i % 5 === 0) {
+      console.log('Fizz');
     } else {
       console.log(i);
     }
+
+    const squareDom = document.createElement('div');
+    squareDom.classList.add('square');
+    squareDom.append(i);
+    containerDom.append(squareDom);
+
   }
+  
